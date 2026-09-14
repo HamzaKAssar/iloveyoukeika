a8cdd66ca3de2bdb192e4bbf158425f345ad7dbb
Hamza Assar <hamzaka.assar@gmail.com>
2026-09-14 14:14:18 +0300

    Change GIFs to local images from images/ folder

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
 const gifStages = [
-    "https://media.tenor.com/EBV7OT7ACfwAAAAj/u-u-qua-qua-u-quaa.gif",    // 0 normal
-    "https://media1.tenor.com/m/uDugCXK4vI4AAAAd/chiikawa-hachiware.gif",  // 1 confused
-    "https://media.tenor.com/f_rkpJbH1s8AAAAj/somsom1012.gif",             // 2 pleading
-    "https://media.tenor.com/OGY9zdREsVAAAAAj/somsom1012.gif",             // 3 sad
-    "https://media1.tenor.com/m/WGfra-Y_Ke0AAAAd/chiikawa-sad.gif",       // 4 sadder
-    "https://media.tenor.com/CivArbX7NzQAAAAj/somsom1012.gif",             // 5 devastated
-    "https://media.tenor.com/5_tv1HquZlcAAAAj/chiikawa.gif",               // 6 very devastated
-    "https://media1.tenor.com/m/uDugCXK4vI4AAAAC/chiikawa-hachiware.gif"  // 7 crying runaway
+    "images/stage-0-normal.png",      // 0 normal
+    "images/stage-1-confused.png",    // 1 confused
+    "images/stage-2-pleading.png",    // 2 pleading
+    "images/stage-3-sad.png",         // 3 sad
+    "images/stage-4-sadder.png",      // 4 sadder
+    "images/stage-5-devastated.png",  // 5 devastated
+    "images/stage-6-very-devastated.png", // 6 very devastated
+    "images/stage-7-crying.png"       // 7 crying runaway
 ]
 
 const noMessages = [
@@ -24,7 +24,7 @@ const noMessages = [
 const yesTeasePokes = [
     "et2ly shwya tab",
     "et2ly aktr",
-    "baolkkkk et2lyy,
+    "baolkkkk et2lyy",
     "ET@LY BAAAA"
 ]
 
